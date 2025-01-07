bb901942fd8cc5110298b0650f388d2015c5eb2c
juanluserra <juanlu.serradilla@gmail.com>
2025-01-07 13:23:40 +0100

    eliminar archivos escritos en el .gitignore

--- APRENDIZAJE ESTADISTICO/Parte 1/practica5/practica5.R
+++
@@ -1,9 +0,0 @@
-library(ISLR)
-library(boot)
-
-set.seed(1)
-train <- sample(392, 196)
-lm.fit <- lm(mpg ~ horsepower, data = Auto, subset = train)
-
-attach(Auto)
-mean((mpg - predict(lm.fit, Auto))[-train]^2)
--- APRENDIZAJE ESTADISTICO/Parte 1/test.R
+++
@@ -1,3 +0,0 @@
-plot(1:10)
-plot(10:1)
-View(iris)
